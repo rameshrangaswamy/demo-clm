1105e619035b9212f507ecd859f408c6fbfefc13
ramesh.rangaswamy <ramesh.rangaswamy@unilever.com>
2020-03-03 19:32:42 +0530

    test

---
+++ sampleclm.java
@@ -0,0 +1,9 @@
+//Explicitly called script for extracting Contract Documents from CLM System and place it in BASIS FTP Folder
+/*
+	Author: 			Devops poc
+	Date Created:		12-Sept-19
+	Last changed by:	Devops poc	
+	Last changed date:	12-Sept-19
+*/
+//Sample script
+test1
